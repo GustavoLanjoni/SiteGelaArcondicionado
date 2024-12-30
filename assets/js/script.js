5f5652831bec73dc6741c1370e53d303c9e79893
Gustavo Lanjoni <gustavosilva94514@gmail.com>
2024-12-30 11:35:52 -0300

    Atualização

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,17 @@ function toggleMenu() {
     const nav = document.querySelector('header nav');
     nav.classList.toggle('active');
 }
+
+// Selecione os elementos necessários
+const expandTrigger = document.querySelector('.expand-trigger');
+const content = document.querySelector('.diferenciais-content');
+const arrow = document.querySelector('.arrow');
+
+// Adiciona o evento de clique ao trigger de expandir
+expandTrigger.addEventListener('click', () => {
+    // Adiciona ou remove a classe para mostrar/esconder o conteúdo
+    content.classList.toggle('show');
+    
+    // Rotaciona a seta quando o conteúdo for exibido
+    arrow.classList.toggle('rotate');
+});
